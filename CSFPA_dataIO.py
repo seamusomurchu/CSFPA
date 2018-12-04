fixed sampling, needed to average the pixel values. still need to find total intensity problem

CSFPA_dataIO.py
@@ -102,42 +102,42 @@ def getXYcoords(f, vtxs):
             cntj = cntj + 1 
         
         #Do for Magnitude X
-        MagXsum = sum(MagXlist)
+        MagXsum = sum(MagXlist)/len(MagXlist)
         MagXarr = np.append(MagXarr,MagXsum)    #Now set int and arr to zero for next loop
         MagXsum = 0
         MagXlist = np.array([])
         #Do for Phase X
-        PhaXsum = sum(PhaXlist)
+        PhaXsum = sum(PhaXlist)/len(PhaXlist)
         PhaXarr = np.append(PhaXarr,PhaXsum)
         PhaXsum = 0
         PhaXlist = np.array([])
         #Do for Mag Y
-        MagYsum = sum(MagYlist)
+        MagYsum = sum(MagYlist)/len(MagYlist)
         MagYarr = np.append(MagYarr,MagYsum)    #Now set int and arr to zero for next loop
         MagYsum = 0
         MagYlist = np.array([])       
         #Do for Phase Y
-        PhaYsum = sum(PhaYlist)
+        PhaYsum = sum(PhaYlist)/len(PhaYlist)
         PhaYarr = np.append(PhaYarr,PhaYsum)
         PhaYsum = 0
         PhaYlist = np.array([])
         #Re, Im data
-        ReXsum = sum(ReXlist)
+        ReXsum = sum(ReXlist)/len(ReXlist)
         ReXarr = np.append(ReXarr,ReXsum)
         ReXsum = 0
         ReXlist = np.array([])
         #ImX arr work
-        ImXsum = sum(ImXlist)
+        ImXsum = sum(ImXlist)/len(ImXlist)
         ImXarr = np.append(ImXarr,ImXsum)
         ImXsum = 0
         ImXlist = np.array([])
         #Re Y data
-        ReYsum = sum(ReYlist)
+        ReYsum = sum(ReYlist)/len(ReYlist)
         ReYarr = np.append(ReYarr,ReYsum)
         ReYsum = 0
         ReYlist = np.array([])
         #ImY arr work
-        ImYsum = sum(ImYlist)
+        ImYsum = sum(ImYlist)/len(ImYlist)
         ImYarr = np.append(ImYarr,ImYsum)
         ImYsum = 0
         ImYlist = np.array([])

CSFPA_plots.py
@@ -384,7 +384,7 @@ def MagXCompPlot(pkl1,pkl2):
 			#print "radius test", np.sqrt(PixCenX[i]**2 + PixCenY[i]**2)
 			#plt.scatter(PixCenX[i]*1000,PixCenY[i]*1000, c=comp[i], cmap='jet',marker='s')
 
-	plt.scatter(PixCenX*1000,PixCenY*1000, c=comp, cmap='jet',marker='s')
+	plt.scatter(PixCenX*1000,PixCenY*1000, c=comp, cmap='jet',marker='s',s=5)
 	plt.axis([-60, 60, -60, 60])
 	plt.axis('equal')
 	plt.title("Data Comparison",fontsize=10)	