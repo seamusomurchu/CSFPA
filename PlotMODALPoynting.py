aaaf55ab31c13c8ac24766bd206732ab2d9a76bf
seamusomurchu <james.murphy.2018@mumail.ie>
2019-03-12 16:22:03 +0000

    added mag x plotter loads raw modal file and qb grasp file

--- PlotMODALPoynting.py
+++ PlotMODALPoynting.py
@@ -12,13 +12,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from CSFPA_dataIO import GetMODALGridPixArea, GridPowerCalc, RetrieveVars
 
-mpath = '/home/james/Downloads/CS1_250219.dat'
-#fpath = '/home/james/Downloads/CS1 0.05 m.dat'
-
+mpath = '/home/james/Downloads/CS1FP241x241.dat'
+#mpath = '/home/james/Downloads/CS1_250219.dat'
+#mpath = '/home/james/Downloads/CS1 0.05 m.dat'
+#mpath = '/home/james/Downloads/CS18319.dat'
+#mpath = '/home/james/Downloads/CS1_cal.dat'
 #mpath = '/home/james/Downloads/CS1.dat'
-gpath = '/home/james/files4CSFPA/qbdataioOUTFILES/FPA_objs_CF1PTDofflinY_CF.qb.pkl'
-gfile = 'FPA_objs_CF1PTDofflinY_CF.qb.pkl'
-
+#gpath = '/home/james/files4CSFPA/qbdataioOUTFILES/FPA_objs_CF1PTDofflinY_CF.qb.pkl'
+#gfile = 'FPA_objs_CF1PTDofflinY_CF.qb.pkl'
+#gpath = '/home/james/files4CSFPA/qbdataioOUTFILES/FPA_objs_CFtestLinX05m_CF.qb.pkl'
+#gfile = 'FPA_objs_CFtestLinX05m_CF.qb.pkl'
+#gpath = '/home/james/files4CSFPA/qbdataioOUTFILES/FPA_objs_CFtest_CF.qb.pkl'
+#gfile = 'FPA_objs_CFtest_CF.qb.pkl'
+gqbfile = '/home/james/files4CSFPA/Fromqbdataio/CF1linx_CF.qb'
 def PoynPlot(fpath):
 	df = pd.read_csv(fpath, sep='\t', header=0)
 	
@@ -51,20 +57,48 @@ def PoynICompPlot(mpath, gpath):
 	fig = plt.figure(facecolor='xkcd:pale green')
 	fig.suptitle("FP Comparison")
 	
-	ax1 = fig.add_subplot(221, facecolor='#d8dcd6')
+	ax1 = fig.add_subplot(221, facecolor='#d8dcd6', aspect='equal')
 	ax1.set_title("MODAL FP from Poynting Z")					   
-   	sc = ax1.scatter(df.X, df.Y, c=df.PoynZ*parea/fourpi, cmap='jet', marker='.')
+   	sc = ax1.scatter(df.X, df.Y, c=df.PoynZ*parea, cmap='jet', marker='.')
 	cbar = fig.colorbar(sc, label="MODAL Poynting (W)")
 	
-	ax2 = fig.add_subplot(222, facecolor='#d8dcd6')
+	ax2 = fig.add_subplot(222, facecolor='#d8dcd6', aspect='equal')
 	ax2.set_title("GRASP FP from Total Intensity")					   
-	sc = ax2.scatter(xycoords[:,0],xycoords[:,1], c=GPow, cmap='jet', marker='.')	
+	sc = ax2.scatter(xycoords[:,0]*1000, xycoords[:,1]*1000, c=GPow, cmap='jet', marker='.')	
 	cbar = fig.colorbar(sc, label="GRASP Total Intensity (W)")	
 	#plot %diff between models
-	ax3 = fig.add_subplot(212, facecolor='#d8dcd6', aspect='equal')
+	ax3 = fig.add_subplot(223, facecolor='#d8dcd6', aspect='equal')
 	ax3.set_title("% Difference between Softwares")	
-	sc = ax3.scatter(xycoords[:,0],xycoords[:,1], c=comp, cmap='RdPu', marker='.')
-	cbar = fig.colorbar(sc, label="% Difference")				            
+	sc = ax3.scatter(xycoords[:,0]*1000, xycoords[:,1]*1000, c=comp, cmap='PiYG', marker='.') #Both useful PiYG & RdPu
+	cbar = fig.colorbar(sc, label="% Difference")	
+#plot histogram of %diff per pixel
+	ax4 = fig.add_subplot(224, facecolor='#d8dcd6')	
+	ax4.set_title("% Difference Histogram")
+	n, bins, patches = ax4.hist(comp)
+	print "hist data", n, bins, patches			            
    #calcualte sum of power
-	print "Poynting Sum W = ", sum(df.PoynZ*parea), "% Diff from 4pi = ", ((sum(df.PoynZ*parea)- fourpi) / fourpi) * 100
+	print "Poynting Sum W = ", sum(df.PoynZ*parea), "% Diff from 1 W = ", ((sum(df.PoynZ*parea) - 1) / 1) * 100
+	return
+
+def MagXCompPlot(mpath, gqbfile):
+	#prep MODAL data
+	df = pd.read_csv(mpath, sep='\t', header=0)
+	garea, parea = GetMODALGridPixArea(mpath)
+	#MagXY = df.MagX.add(df.MagY, fill_value=0) will give intensity pattern
+	#prep GRASP
+	dfg = pd.read_csv(gqbfile, sep='\t', header=0)
+	#Do Plot
+	fig = plt.figure(facecolor='xkcd:pale green')
+	fig.suptitle("FP MagX Comparison")
+	
+	ax1 = fig.add_subplot(221, facecolor='#d8dcd6', aspect='equal')
+	ax1.set_title("MODAL FP Magnitude X")					   
+   	sc = ax1.scatter(df.X, df.Y, c=df.MagX, cmap='jet', marker='.')
+	cbar = fig.colorbar(sc, label="MagX")	
+	
+	ax2 = fig.add_subplot(222, facecolor='#d8dcd6', aspect='equal')
+	ax2.set_title("GRASP FP MagX (qbfile)")					   
+	sc = ax2.scatter(dfg.Xpos*1000, dfg.Ypos*1000, c=dfg.Yamp, cmap='jet', marker='.')	
+	cbar = fig.colorbar(sc, label="GRASP X Amp")	
+	
 	return
